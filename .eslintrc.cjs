3bf047e0e43cad153a29f13a477480e463d30253
tusxxx <tusxdie@gmail.com>
2024-10-03 20:05:04 +0700

    deploy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
@@ -13,3 +16,4 @@ module.exports = {
     ecmaVersion: 'latest'
   }
 }
+
